8d2aa1e335125a6a7fd9ce2b1a87aa02b3338224
toga_ae36 <toga_ae36@18-8S>
2017-12-03 20:07:21 +0400

    icamelcase diag

--- Analyzer1/Analyzer1/Analyzer1/CodeFixProvider.cs
+++ Analyzer1/Analyzer1/Analyzer1/CodeFixProvider.cs
@@ -18,11 +18,11 @@ namespace Analyzer1
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(Analyzer1CodeFixProvider)), Shared]
     public class Analyzer1CodeFixProvider : CodeFixProvider
     {
-        private const string title = "Make uppercase";
+        private const string title = "Make ICamelCase";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
-            get { return ImmutableArray.Create(Analyzer1Analyzer.DiagnosticId); }
+            get { return ImmutableArray.Create(InterfaceAnalyzer.DiagnosticId); }
         }
 
         public sealed override FixAllProvider GetFixAllProvider()
--- Analyzer1/Analyzer1/Analyzer1/DiagnosticAnalyzer.cs
+++ Analyzer1/Analyzer1/Analyzer1/DiagnosticAnalyzer.cs
@@ -12,58 +12,17 @@ using System.Text.RegularExpressions;
 namespace Analyzer1
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class Analyzer1Analyzer : DiagnosticAnalyzer
+    public class InterfaceAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "Analyzer1";
-
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        public const string DiagnosticId = "InterfaceAnalyzer";       
         private const string Category = "Naming";
-
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
-
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, "Type name doesn't match ICamelCase", "Type name '{0}' doesn't match ICamelCase", Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: "Type names should match ICamelCase.");
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
-        {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbolNamedType, SymbolKind.NamedType);
+        {           
             context.RegisterSymbolAction(AnalyzeSymbolInterface, SymbolKind.NamedType);
         }
-
-        private static void AnalyzeSymbolNamedType(SymbolAnalysisContext context)
-        {
-            Regex UpperCamelCase = new Regex("[A-Z][a-zA-Z0-9]*");
-            Regex IUpperCamelCase = new Regex("I[A-Z][a-zA-Z0-9]*");
-            Regex TUpperCamelCase = new Regex("T[A-Z][a-zA-Z0-9]*");
-            Regex lowerCamelCase = new Regex("[a-z][a-zA-Z0-9]*");
-            Regex _lowerCamelCase = new Regex("_[a-z][a-zA-Z0-9]*");
-            Regex defaultPattern = UpperCamelCase;
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            if (context.Symbol as ITypeParameterSymbol!=null)
-            {
-                defaultPattern = TUpperCamelCase;
-            }
-            else if (context.Symbol as ILocalSymbol != null)
-            {
-                defaultPattern = lowerCamelCase;
-            }
-            if (!defaultPattern.IsMatch(context.Symbol.Name))
-           {
-               // For all such symbols, produce a diagnostic.
-               //iydioqwe
-               var diagnostic = Diagnostic.Create(Rule, context.Symbol.Locations[0], context.Symbol.Name);
-           
-               context.ReportDiagnostic(diagnostic);
-           }
-
-            
-        }
-
 
         private static void AnalyzeSymbolInterface(SymbolAnalysisContext context)
         {
